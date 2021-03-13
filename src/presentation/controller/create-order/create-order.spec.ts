8a55f06a62d10ed160cebcc36532fa2fac072b5d
João Pedro <joaopedrobc@gmail.com>
2021-03-13 05:23:43 -0300

    test: ensure CreateOrderController throws an server error if CreateOrder throws an error

--- src/presentation/controller/create-order/create-order.spec.ts
+++ src/presentation/controller/create-order/create-order.spec.ts
@@ -1,13 +1,45 @@
 import { CreateOrderController } from './create-order'
-import { InvalidParamError, MissingParamError } from '../../error'
+import { InvalidParamError, MissingParamError, ServerError } from '../../error'
+import { CreateOrder, CreateOrderModel } from '../../../domain/usecase/create-order'
+import { OrderModel } from '../../../domain/model/order'
 
-const makeSut = (): CreateOrderController => {
-  return new CreateOrderController()
+const makeCreateOrder = (): CreateOrder => {
+  class CreateOrderStub implements CreateOrder {
+    async create (order: CreateOrderModel): Promise<OrderModel> {
+      const fakeOrder = {
+        id: 'valid_id',
+        products: [
+          {
+            name: 'valid_product_name',
+            quantity: 1,
+            price: 10
+          }
+        ],
+        total: 10
+      }
+
+      return await new Promise(resolve => resolve(fakeOrder))
+    }
+  }
+
+  return new CreateOrderStub()
+}
+
+interface SutType {
+  sut: CreateOrderController
+  createOrderStub: CreateOrder
+}
+
+const makeSut = (): SutType => {
+  const createOrderStub = makeCreateOrder()
+  const sut = new CreateOrderController(createOrderStub)
+
+  return { sut, createOrderStub }
 }
 
 describe('Create Order Controller', () => {
   test('Should return 400 if no products are provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {}
     }
@@ -17,14 +49,34 @@ describe('Create Order Controller', () => {
   })
 
   test('Should return 400 if products are provided but are empty', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
-        products: {}
+        products: []
       }
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('products'))
+  })
+
+  test('Should return 500 if CreateOrder throws', async () => {
+    const { sut, createOrderStub } = makeSut()
+    jest.spyOn(createOrderStub, 'create').mockImplementationOnce(async () => {
+      return await new Promise((resolve, reject) => reject(new Error()))
+    })
+    const httpRequest = {
+      body: {
+        products: [
+          {
+            name: 'any_name',
+            quantity: 0
+          }
+        ]
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
