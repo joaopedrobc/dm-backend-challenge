7a6b666cd6ead968daaee2e2b1f8ee4ec3ca9870
João Pedro <joaopedrobc@gmail.com>
2021-03-13 04:09:28 -0300

    refactor: change CreateOrderController to implements Controller

--- src/presentation/controller/create-order/create-order.spec.ts
+++ src/presentation/controller/create-order/create-order.spec.ts
@@ -6,12 +6,12 @@ const makeSut = (): CreateOrderController => {
 }
 
 describe('Create Order Controller', () => {
-  test('Should return 400 if no products are provided', () => {
+  test('Should return 400 if no products are provided', async () => {
     const sut = makeSut()
     const httpRequest = {
       body: {}
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('products'))
   })
--- src/presentation/controller/create-order/create-order.ts
+++ src/presentation/controller/create-order/create-order.ts
@@ -1,8 +1,8 @@
 import { MissingParamError } from '../../error'
-import { HttpRequest, HttpResponse } from '../../protocol'
+import { Controller, HttpRequest, HttpResponse } from '../../protocol'
 
-export class CreateOrderController {
-  handle (httpRequest: HttpRequest): HttpResponse {
+export class CreateOrderController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     return {
       statusCode: 400,
       body: new MissingParamError('products')
