6239ac3b3312654c7aacfb029bf3bf722ff4b402
João Pedro <joaopedrobc@gmail.com>
2021-03-13 04:28:23 -0300

    feat: ensure CreateOrderController throws an error if products are provided but are an empyt list

--- src/presentation/controller/create-order/create-order.ts
+++ src/presentation/controller/create-order/create-order.ts
@@ -1,11 +1,23 @@
-import { MissingParamError } from '../../error'
+import { InvalidParamError, MissingParamError } from '../../error'
 import { Controller, HttpRequest, HttpResponse } from '../../protocol'
+import { badRequest, ok } from '../../helper/http-helper'
 
 export class CreateOrderController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return {
-      statusCode: 400,
-      body: new MissingParamError('products')
+    const { body } = httpRequest
+
+    const requiredFields = ['products']
+    for (const field of requiredFields) {
+      if (!body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
+
+    const { products } = body
+    if (products && Object.keys(products).length === 0) {
+      return badRequest(new InvalidParamError('products'))
+    }
+
+    return ok({})
   }
 }
