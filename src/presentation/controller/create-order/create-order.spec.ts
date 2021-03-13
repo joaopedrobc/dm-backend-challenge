dd7397b36aa6745454f440474513232881ced60b
João Pedro <joaopedrobc@gmail.com>
2021-03-13 04:29:23 -0300

    test: ensure CreateOrderController throws an error if products are provided but are an empyt list

--- src/presentation/controller/create-order/create-order.spec.ts
+++ src/presentation/controller/create-order/create-order.spec.ts
@@ -1,5 +1,5 @@
 import { CreateOrderController } from './create-order'
-import { MissingParamError } from '../../error'
+import { InvalidParamError, MissingParamError } from '../../error'
 
 const makeSut = (): CreateOrderController => {
   return new CreateOrderController()
@@ -14,5 +14,17 @@ describe('Create Order Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('products'))
+  })
+
+  test('Should return 400 if products are provided but are empty', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        products: {}
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('products'))
   })
 })
