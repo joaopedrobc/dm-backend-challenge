574453fba4c3169a9232ca37fa2670f1eed6f315
João Pedro <joaopedrobc@gmail.com>
2021-03-14 17:11:16 -0300

    test: ensures calls FindProductRepository with correct values

--- src/data/usecases/create-order/db-create-order.spec.ts
+++ src/data/usecases/create-order/db-create-order.spec.ts
@@ -1,6 +1,9 @@
 import { OrderModel } from '../../../domain/models/order'
+import { ProductModel } from '../../../domain/models/product'
 import { CreateOrderModel } from '../../../domain/usecases/create-order'
+import { FindProductModel } from '../../../domain/usecases/find-product'
 import { CreateOrderRepository } from '../../protocols/create-order-repository'
+import { FindProductRepository } from '../../protocols/find-product-repository'
 import { DbCreateOrder } from './db-create-order'
 
 const makeCreateOrderRepository = (): CreateOrderRepository => {
@@ -10,6 +13,7 @@ const makeCreateOrderRepository = (): CreateOrderRepository => {
         id: 'valid_id',
         products: [
           {
+            id: 'any_id',
             name: 'any_name',
             quantity: 0,
             price: 10
@@ -23,73 +27,88 @@ const makeCreateOrderRepository = (): CreateOrderRepository => {
   return new CreateOrderRepositoryStub()
 }
 
+const makeFindProductRepository = (): FindProductRepository => {
+  class FindProductRepositoryStub implements FindProductRepository {
+    async find (productData: FindProductModel): Promise<ProductModel> {
+      const fakeProduct = {
+        id: 'any_id',
+        name: 'any_name',
+        quantity: 0,
+        price: 10
+      }
+      return new Promise(resolve => resolve(fakeProduct))
+    }
+  }
+
+  return new FindProductRepositoryStub()
+}
+
 interface SutType {
   sut: DbCreateOrder
   createOrderRepositoryStub: CreateOrderRepository
+  findProductRepositoryStub: FindProductRepository
 }
 
 const makeSut = (): SutType => {
-  const createOrderRepositoryStub = makeCreateOrderRepository()
-  const sut = new DbCreateOrder(createOrderRepositoryStub)
+  const createOrderRepositoryStub: CreateOrderRepository = makeCreateOrderRepository()
+  const findProductRepositoryStub: FindProductRepository = makeFindProductRepository()
+  const sut = new DbCreateOrder(createOrderRepositoryStub, findProductRepositoryStub)
   return {
     sut,
-    createOrderRepositoryStub
+    createOrderRepositoryStub,
+    findProductRepositoryStub
   }
 }
+
+const makeFakeOrder = (): CreateOrderModel => ({
+  products: [
+    {
+      name: 'any_name',
+      quantity: 0
+    },
+    {
+      name: 'another_name',
+      quantity: 2
+    }
+  ]
+})
 
 describe('DbCreateOrder Usecase', () => {
   test('Should call CreateOrderRepository with correct values', async () => {
     const { sut, createOrderRepositoryStub } = makeSut()
     const createSpy = jest.spyOn(createOrderRepositoryStub, 'create')
-    const orderData = {
-      products: [
-        {
-          name: 'any_name',
-          quantity: 0
-        }
-      ]
-    }
+    const orderData = makeFakeOrder()
     await sut.create(orderData)
-    expect(createSpy).toHaveBeenCalledWith({
-      products: [
-        {
-          name: 'any_name',
-          quantity: 0
-        }
-      ]
-    })
+    expect(createSpy).toHaveBeenCalledWith(orderData)
   })
 
   test('Should throw if CreateOrderRepository throws', async () => {
     const { sut, createOrderRepositoryStub } = makeSut()
     jest.spyOn(createOrderRepositoryStub, 'create').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
-    const orderData = {
-      products: [
-        {
-          name: 'any_name',
-          quantity: 0
-        }
-      ]
-    }
+    const orderData = makeFakeOrder()
     const promise = sut.create(orderData)
     await expect(promise).rejects.toThrow()
   })
 
+  test('Should call FindProductRepository with correct values', async () => {
+    const { sut, findProductRepositoryStub } = makeSut()
+    const findSpy = jest.spyOn(findProductRepositoryStub, 'find')
+    const orderData = makeFakeOrder()
+    await sut.create(orderData)
+    expect(findSpy).toHaveBeenCalledTimes(2)
+    expect(findSpy).toHaveBeenCalledWith({ name: 'any_name' })
+    expect(findSpy).toHaveBeenCalledWith({ name: 'another_name' })
+  })
+
   test('Should return an order on success', async () => {
     const { sut } = makeSut()
-    const orderData = {
-      products: [
-        {
-          name: 'any_name',
-          quantity: 0
-        }
-      ]
-    }
+    const orderData = makeFakeOrder()
     const order = await sut.create(orderData)
     expect(order).toEqual({
       id: 'valid_id',
       products: [
         {
+          id: 'any_id',
           name: 'any_name',
           quantity: 0,
           price: 10
