f90dda2760c80d1f8de1908d417a07943401cd50
João Pedro <joaopedrobc@gmail.com>
2021-03-15 02:39:28 -0300

    feat: ensures ProductMongoRepository returns a product with update values after update

--- src/infra/db/mongodb/product-repository/product.ts
+++ src/infra/db/mongodb/product-repository/product.ts
@@ -1,12 +1,21 @@
 import { FindProductRepository } from '../../../../data/protocols/find-product-repository'
+import { UpdateProductRepository } from '../../../../data/protocols/update-protocol-repository'
 import { ProductModel } from '../../../../domain/models/product'
 import { FindProductModel } from '../../../../domain/usecases/find-product'
+import { UpdateProductModel } from '../../../../domain/usecases/update-product'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class ProductMongoRepository implements FindProductRepository {
+export class ProductMongoRepository implements FindProductRepository, UpdateProductRepository {
   async find (productData: FindProductModel): Promise<ProductModel> {
     const orderCollection = MongoHelper.getCollection('products')
     const result = await orderCollection.findOne(productData)
     return MongoHelper.mapWithoutId(result)
+  }
+
+  async update (name: FindProductModel, productData: UpdateProductModel): Promise<ProductModel> {
+    const orderCollection = MongoHelper.getCollection('products')
+    const newValues = { $set: productData }
+    await orderCollection.updateOne(name, newValues)
+    return this.find({ name: productData.name })
   }
 }
