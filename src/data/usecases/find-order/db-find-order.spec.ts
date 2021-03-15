9231b168ce3814fb543c76e953e8bb01009d4f2a
João Pedro <joaopedrobc@gmail.com>
2021-03-15 04:35:35 -0300

    test: ensures return orders on success

--- src/data/usecases/find-order/db-find-order.spec.ts
+++ src/data/usecases/find-order/db-find-order.spec.ts
@@ -1,12 +1,27 @@
-import { OrderModel } from '../../../domain/models/order'
+import { ListOrderModel } from '../../../domain/models/list-order'
 import { FindOrderModel, FindOrderRepository } from '../../protocols/find-order-repository'
 import { DbFindOrder } from './db-find-order'
 
 describe('DbFindOrder Usecase', () => {
   const makeFindOrderRepositoryStub = (): FindOrderRepository => {
     class FindOrderRepositoryStub implements FindOrderRepository {
-      async find (productData: FindOrderModel): Promise<OrderModel[]> {
-        return new Promise(resolve => resolve(null))
+      async find (id: FindOrderModel): Promise<ListOrderModel> {
+        const fakeData: ListOrderModel = {
+          orders: [
+            {
+              id: 'any_id',
+              products: [
+                {
+                  name: 'Watermelon',
+                  quantity: 2,
+                  price: 5.47
+                }
+              ],
+              total: 10.94
+            }
+          ]
+        }
+        return new Promise(resolve => resolve(fakeData))
       }
     }
 
@@ -33,5 +48,25 @@ describe('DbFindOrder Usecase', () => {
     jest.spyOn(findOrderRepositoryStub, 'find').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const promise = sut.find({})
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return orders on success', async () => {
+    const { sut } = makeSut()
+    const order = await sut.find({})
+    expect(order).toEqual({
+      orders: [
+        {
+          id: 'any_id',
+          products: [
+            {
+              name: 'Watermelon',
+              quantity: 2,
+              price: 5.47
+            }
+          ],
+          total: 10.94
+        }
+      ]
+    })
   })
 })
