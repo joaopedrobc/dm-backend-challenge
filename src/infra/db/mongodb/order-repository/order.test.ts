9f368c5401345ee5dcf6f0e119a47e822c4cbd2b
João Pedro <joaopedrobc@gmail.com>
2021-03-14 21:42:22 -0300

    test: ensures OderMongoRepository returnas an order on success

---
+++ src/infra/db/mongodb/order-repository/order.test.ts
@@ -0,0 +1,32 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { OrderMongoRepository } from './order'
+
+describe('Order Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an order on success', async () => {
+    const sut = new OrderMongoRepository()
+    const order = await sut.create({
+      products: [
+        {
+          name: 'Kiwi',
+          quantity: 2,
+          price: 9.71
+        }
+      ],
+      total: 19.42
+    })
+    expect(order).toBeTruthy()
+    expect(order.id).toBeTruthy()
+    expect(order.products).toBeTruthy()
+    expect(order.products.length).toBe(1)
+    expect(order.total).toBeTruthy()
+    expect(order.total).toBe(19.42)
+  })
+})
