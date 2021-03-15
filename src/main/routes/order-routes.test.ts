db7b8210d4658a550f4ee7ec20fde3d38564f73d
João Pedro <joaopedrobc@gmail.com>
2021-03-14 22:43:51 -0300

    refactor: connect to mongodb before integration test

--- src/main/routes/order-routes.test.ts
+++ src/main/routes/order-routes.test.ts
@@ -1,7 +1,21 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('Order Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const orderCollection = await MongoHelper.getCollection('orders')
+    await orderCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/orders')
