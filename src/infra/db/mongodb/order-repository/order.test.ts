ae2ffe8408f31080c6501fa84d231056cbae3454
João Pedro <joaopedrobc@gmail.com>
2021-03-15 04:48:33 -0300

    feat: ensures return orders on success

--- src/infra/db/mongodb/order-repository/order.test.ts
+++ src/infra/db/mongodb/order-repository/order.test.ts
@@ -38,4 +38,27 @@ describe('Order Mongo Repository', () => {
     expect(order.total).toBeTruthy()
     expect(order.total).toBe(19.42)
   })
+
+  test('Should return orders on success', async () => {
+    const sut = makeSut()
+    await sut.create({
+      products: [
+        {
+          name: 'Kiwi',
+          quantity: 2,
+          price: 9.71
+        },
+        {
+          name: 'Orange',
+          quantity: 3,
+          price: 2.71
+        }
+      ],
+      total: 27.55
+    })
+    const result = await sut.find({})
+    expect(result.orders).toBeTruthy()
+    expect(result.orders.length).toBe(1)
+    expect(result.orders[0].products.length).toBe(2)
+  })
 })
--- src/infra/db/mongodb/order-repository/order.ts
+++ src/infra/db/mongodb/order-repository/order.ts
@@ -1,11 +1,24 @@
 import { CreateFullOrderModel, CreateOrderRepository } from '../../../../data/protocols/create-order-repository'
+import { FindOrderModel, FindOrderRepository } from '../../../../data/protocols/find-order-repository'
+import { ListOrderModel } from '../../../../domain/models/list-order'
 import { OrderModel } from '../../../../domain/models/order'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class OrderMongoRepository implements CreateOrderRepository {
+export class OrderMongoRepository implements CreateOrderRepository, FindOrderRepository {
   async create (orderData: CreateFullOrderModel): Promise<OrderModel> {
     const orderCollection = MongoHelper.getCollection('orders')
     const result = await orderCollection.insertOne(orderData)
     return MongoHelper.map(result.ops[0])
+  }
+
+  async find (id: FindOrderModel): Promise<ListOrderModel> {
+    const orderCollection = MongoHelper.getCollection('orders')
+    const result = await orderCollection.find(id)
+    const orders = await result.toArray()
+    const ordersMapped = orders.map(order => (
+      MongoHelper.map(order)
+    ))
+
+    return MongoHelper.map({ orders: ordersMapped })
   }
 }
