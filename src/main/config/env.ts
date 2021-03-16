fe89d445f43b52ce4eb0d58b2f3d2b810ed4155c
João Pedro <joaopedrobc@gmail.com>
2021-03-16 07:45:45 -0300

    refactor: change env file structure

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,13 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017',
-  database: process.env.DATABASE || 'dm-challenge',
-  port: process.env.PORT || 5050
+  mongo: {
+    url: process.env.MONGO_URL || 'mongodb://localhost:27017',
+    database: process.env.DATABASE || 'dm-challenge',
+    port: process.env.PORT || 3000
+  },
+  amqp: {
+    url: process.env.AMQP_URL || 'amqp://localhost',
+    exchange: process.env.AMQP_EXCHANGE || 'stock',
+    queues: process.env.AMQP_QUEUES || 'incremented,decremented'
+  }
+
 }
